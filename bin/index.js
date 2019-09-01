3e4d232de9fa900555fa324b27bd442957ecc21d
Gabriel <gabrielperales@gmail.com>
2019-09-02 01:22:19 +0200

    updated parser options and dependencies

--- bin/index.js
+++ bin/index.js
@@ -1,53 +1,12 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const yaml = require('js-yaml');
-const {promisify} = require('util');
+const { promisify } = require('util');
 
 const writeFileAsync = promisify(fs.writeFile);
-const inquirer = require('inquirer');
 
-const defaults = require('./defaults');
-const {
-  mainOptions,
-  propertiesOptions,
-  editPropertiesOptions,
-  fileOptions,
-} = require('./questions');
+const cli = require('../lib/cli');
 
-inquirer
-  .prompt(mainOptions)
-  .then(
-    ({type}) =>
-      type === 'custom'
-        ? inquirer
-            .prompt(propertiesOptions)
-            .then(({properties}) =>
-              inquirer.prompt(
-                editPropertiesOptions.filter(
-                  question => properties.indexOf(question.name) !== -1,
-                ),
-              ),
-            )
-        : defaults,
-  )
-  .then(options =>
-    inquirer.prompt(fileOptions).then(({output}) => {
-      const content = (() => {
-        switch (output) {
-          case 'json':
-            return JSON.stringify(options, null, 2);
-          case 'yml':
-            return yaml.dump(options);
-          default:
-            throw new Error('file extension not valid');
-        }
-      })();
-
-      return {extension: output, content};
-    }),
-  )
-  .then(({extension, content}) =>
-    writeFileAsync(`.prettierrc.${extension}`, content),
-  )
+cli
+  .then(({ extension, content }) => writeFileAsync(`.prettierrc.${extension}`, content))
   .catch(error => console.error(error));
